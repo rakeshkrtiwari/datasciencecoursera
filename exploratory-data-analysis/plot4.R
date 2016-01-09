efa82b6e29a8af0944ebfba87e002327459077f8
rakeshkrtiwari <rakeshkrtiwari@gmail.com>
2016-01-09 08:38:27 +0530

    change y-axis heading

--- exploratory-data-analysis/plot4.R
+++ exploratory-data-analysis/plot4.R
@@ -21,7 +21,7 @@ generatePlot4 <- function()
     
     plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
     
-    plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+    plot(datetime, subMetering1, type="l", ylab="Energy Sub metering", xlab="")
     lines(datetime, subMetering2, type="l", col="red")
     lines(datetime, subMetering3, type="l", col="blue")
     legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
